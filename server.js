60556d65cd585df65b97a45d6881de4eab475ce0
vipulsingh24 <letsmailvipul@gmail.com>
2017-08-11 02:35:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-   res.send('Article-One is requested and will serve here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function(req, res){
